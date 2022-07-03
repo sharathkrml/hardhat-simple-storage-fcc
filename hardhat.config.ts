b5f43e33855ac0094d5444ca4f325131ada64887
Sharath <sharathkrml@gmail.com>
2022-07-03 20:30:11 +0530

    fix imports and add needed imports at config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,19 @@
-require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
-require("@nomiclabs/hardhat-etherscan");
-require("./tasks/block-number");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
+import "@nomiclabs/hardhat-waffle";
+import "dotenv/config";
+import "@nomiclabs/hardhat-etherscan";
+import "./tasks/block-number";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-ethers";
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,4 @@
-const { ethers, run, network } = require("hardhat");
+import { ethers, run, network } from "hardhat";
 
 async function main() {
   const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
@@ -19,14 +19,14 @@ async function main() {
   const updatedvalue = await simplestorage.retrieve();
   console.log(`Updated value is :${updatedvalue}`);
 }
-const verify = async (contractAddress, args) => {
+const verify = async (contractAddress: string, args: any[]) => {
   console.log("verifying contract....");
   try {
     await run("verify:verify", {
       address: contractAddress,
       constructorArguments: args,
     });
-  } catch (e) {
+  } catch (e: any) {
     console.log(e);
   }
 };
